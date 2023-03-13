0f85f4281fa7398ea7cd87de0da66ab6985d60c7
DrMoE77 <drmo.afifi@gmail.com>
2023-03-13 22:57:44 +1100

    update

---
+++ frontEnd/src/data.js
@@ -0,0 +1,29 @@
+const data ={
+    products: [
+        {
+            name:"Panadol",
+            category:"otc",
+            image: "frontEnd\public\media\PanadolRapid.jpg",
+            description:"analgesic 100 tablets",
+            price:"$4.99"
+            
+        },
+        {
+            name:"Ginko",
+            category:"vitamins",
+            image: "frontEnd\public\media\Ginko.jpg",
+            description:"Memory aid 60 capsules",
+            price:"$25"
+            
+        },
+        {
+            name:"Clear Wipe",
+            category:"fos",
+            image: "frontEnd\public\media\CLearwipe.jpg",
+            description:"lens cleaner",
+            price:"$6.99"
+            
+        },
+    ]
+}
+ export default data;
